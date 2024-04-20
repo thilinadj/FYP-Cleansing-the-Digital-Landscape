bf4048c1d00a18340ad60aa35b16f8cd6abf760e
Thilina Dilanka Jayasuriya <thilinadjayasuriya@gmail.com>
2024-04-21 02:29:44 +0530

    update

--- shared_data/shared_data.py
+++ shared_data/shared_data.py
@@ -1,6 +1,5 @@
 mono_audio_path = r"Dataset-ai\mono_audio"
 # audio_data_path = r"Dataset\mono_audio"
-# audio_data_path = r"tensorflow-speech-recognition-challenge\train\audio"
 
 stereo_audio_path = r"Dataset-ai\stereo_audio"
 # stereo_audio_path = r"Dataset\stereo_audio"
